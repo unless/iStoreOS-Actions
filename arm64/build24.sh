711652ca3706930ccc0add890dcdd3b9bf34e398
unless <jbpv@163.com>
2025-11-01 11:24:36 +0800

    Update build24.sh

--- arm64/build24.sh
+++ arm64/build24.sh
@@ -1062,8 +1062,8 @@ PACKAGES="$PACKAGES luci-app-amlogic luci-i18n-amlogic-zh-cn"
 #PACKAGES="$PACKAGES lucky luci-app-lucky luci-i18n-lucky-zh-cn"
 #PACKAGES="$PACKAGES openlist2 luci-app-openlist2 luci-i18n-openlist2-zh-cn"
 PACKAGES="$PACKAGES luci-app-ramfree luci-i18n-ramfree-zh-cn"
-PACKAGES="$PACKAGES luci-app-adguardhome luci-i18n-adguardhome-zh-cn"
-PACKAGES="$PACKAGES luci-app-microsocks luci-i18n-microsocks-zh-cn"
+#PACKAGES="$PACKAGES luci-app-adguardhome luci-i18n-adguardhome-zh-cn"
+#PACKAGES="$PACKAGES luci-app-microsocks luci-i18n-microsocks-zh-cn"
 #PACKAGES="$PACKAGES libcron daed luci-app-daed luci-i18n-dead-zh-cn"
 
 # opc-rely依赖，其他会自动集成
